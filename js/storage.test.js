265d221604e776c35618485f9b595a0617c67987
DariaMOdermatt <daria.odermatt@bluewin.ch>
2026-06-09 21:35:51 +0200

    Create storage.test.js

---
+++ js/storage.test.js
@@ -0,0 +1,58 @@
+const CACHE = "zyklus-v1";
+
+const LOCAL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.svg",
+  "./css/styles.css",
+  "./js/storage.js",
+  "./js/engine.js"
+];
+
+const CDN = [
+  "https://unpkg.com/react@18/umd/react.production.min.js",
+  "https://unpkg.com/react-dom@18/umd/react-dom.production.min.js",
+  "https://fonts.googleapis.com/css2?family=DM+Sans:opsz,wght@9..40,300;9..40,400;9..40,500&family=Fraunces:opsz,wght@9..144,300;9..144,500;9..144,700&display=swap"
+];
+
+self.addEventListener("install", function (event) {
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      return Promise.allSettled(
+        LOCAL.concat(CDN).map(function (url) {
+          return cache.add(url).catch(function () {});
+        })
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function (event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then(function (cached) {
+      return cached || fetch(event.request).then(function (response) {
+        if (response && response.status === 200) {
+          var clone = response.clone();
+          caches.open(CACHE).then(function (cache) { cache.put(event.request, clone); });
+        }
+        return response;
+      }).catch(function () {
+        return caches.match("./index.html");
+      });
+    })
+  );
+});
